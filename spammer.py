6c0de052be270b4fe5e3e85013d531059f969c73
piupiujk <docs4p@yandex.ru>
2023-02-20 21:04:54 +0500

    Добавил отправщик сообщений в телеграмме

--- spammer.py
+++ spammer.py
@@ -1,8 +1,32 @@
-from social_spam import Mail
-
+from social_spam import Mail, Telegram
 
 mail = Mail()
-mail.connect_mail('Почта с которой отправлять', 'Пароль Приложений')
+tg = Telegram()
 
-mail.set_message('Message from docsap', 'How are you?')
+
+"""Подключение к почте и рассылка писем"""
+
+
+email = 'Почта'
+app_password = 'Пароль приложения'
+
+mail.connect_mail(email, app_password)
+
+mail.set_message('Тема сообщения', 'Сообщение')
 mail.send_message('Почта куда отпарвлять')
+
+
+"""Подключение к тг и рассылка сообщений"""
+
+
+# https://my.telegram.org/apps - сайт где взять всю инфу об тг
+
+api_id = ''
+api_hash = ''
+phone = ''
+
+tg.connect_user(api_id=api_id, api_hash=api_hash, phone_number=phone)
+
+user_id = tg.get_id_by_phone("Номер телефона")  # Что бы узнать id по номеру
+tg.send_message(user_id=user_id, message='Введите сообщение')
+
