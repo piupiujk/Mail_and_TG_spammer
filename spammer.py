3e2a8206cc436d68328135be5d54f974f6d61b57
piupiujk <docs4p@yandex.ru>
2023-02-24 01:40:05 +0500

    Переделал все в функции, добавил какое сообщение должно будет отпарвляться

--- spammer.py
+++ spammer.py
@@ -8,48 +8,56 @@ env.read_env()
 mail = Mail()
 tg = Telegram()
 
-
 """!!!!!!!!!!!!!!!!!!
 Сделать все в функиях
 !!!!!!!!!!!!!!!!!!"""
 
 
-"""Подключение к почте и рассылка писем"""
+def connect_email(email, app_password):
+    """Подключение к почте и рассылка писем"""
+
+    email = env.str('email')
+    app_password = env.str('app_password')
+
+    mail.connect_mail(email, app_password)
+
+    mail.set_message('Тема сообщения', 'Сообщение')
+    mail.send_message('Почта куда отпарвлять')
 
 
-email = env.str('email')
-app_password = env.str('app_password')
+def id_base():
+    """Получение id из csv файла в список"""
+
+    tg_id_base = []
+
+    with open('user_data.csv', encoding='utf-8') as base_file:
+        base_reader = csv.reader(base_file, delimiter=',')
+        for row in base_reader:
+            tg_id_base.append(row[1])
+        tg_id_base.pop(0)
+
+    return tg_id_base
 
 
-mail.connect_mail(email, app_password)
+def connect_tg():
+    """Подключение к тг и рассылка сообщений"""
 
-mail.set_message('Тема сообщения', 'Сообщение')
-mail.send_message('Почта куда отпарвлять')
+    # https://my.telegram.org/apps - сайт где взять всю инфу об тг
 
+    api_id = env.int('api_id')
+    api_hash = env.str('api_hash')
+    phone = env.str('phone')
 
-"""Получение id из csv файла в список"""
+    tg.connect_user(api_id=api_id, api_hash=api_hash, phone_number=phone)
 
-tg_id_base = []
+    # user_id = tg.get_id_by_phone("Номер телефона")  # Что бы узнать id по номеру
+    # tg.send_message(user_id=user_id, message='Введите сообщение')
 
-with open('user_data.csv', encoding='utf-8') as base_file:
-    base_reader = csv.reader(base_file, delimiter=',')
-    for row in base_reader:
-        tg_id_base.append(row[1])
-    tg_id_base.pop(0)
+    tg.set_message("**Крупное обновление в хомяке ☔.**\n\nОбновили дизайн, добавили множество новых и интересных товаров, таких как Баллы Мвидео, Заказы с Яндекс и Деливери за 50% от стоимости, Faceit, Лиры для всех, Турецкие Apple Gift карты и многое другое по самым лучшим ценам, большая часть товаров уже доступна к покупке, оставшиеся зальем совсем скоро, так же в течение недели появится Spotify, Discord Nitro, Спортмастер и многое другое, следите за обновлениями в боте.\n\nПриятных покупок💜")
 
+    tg.set_image("C:/Users/Пользователь/Desktop/python/Projects/Mail_and_TG_spammer/Mail_and_TG_spammer/image_1.jpeg")
 
-"""Подключение к тг и рассылка сообщений"""
-
-
-# https://my.telegram.org/apps - сайт где взять всю инфу об тг
-
-api_id = env.int('api_id')
-api_hash = env.str('api_hash')
-phone = env.str('phone')
+    # tg.start_selective_spam(id_base()) Должно работать, но пока рано запускать на всю базу
 
 
-tg.connect_user(api_id=api_id, api_hash=api_hash, phone_number=phone)
-
-user_id = tg.get_id_by_phone("Номер телефона")  # Что бы узнать id по номеру
-tg.send_message(user_id=user_id, message='Введите сообщение')
 
